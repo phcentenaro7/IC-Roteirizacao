9522cdabf3ed909fea0a5035f7d3d353db497024
phcentenaro7 <phc7blu@gmail.com>
2024-02-22 12:12:14 -0300

    modelo dfj atualizado

--- CNMAC 2024/Modelos/dfj.jl
+++ CNMAC 2024/Modelos/dfj.jl
@@ -18,3 +18,26 @@ function createDFJTSP(model, C)
     end
     set_attribute(model, MOI.LazyConstraintCallback(), callbackRemoveSubcycles)
 end
+
+function findSubcyclesTSP(edges::Vector{Tuple{Int, Int}}, n::Int)
+    unvisited = Set(collect(1:n))
+    shortest = collect(1:n)
+    while !isempty(unvisited)
+        thisCycle, neighbors = Int[], unvisited
+        while !isempty(neighbors)
+            current = pop!(neighbors)
+            push!(thisCycle, current)
+            if length(thisCycle) > 1
+                pop!(unvisited, current)
+            end
+            neighbors =
+                [j for (i, j) in edges if i == current && j in unvisited]
+        end
+        if 1 < length(thisCycle) < length(shortest)
+            shortest = thisCycle
+        end
+    end
+    return shortest
+end
+
+findSubcyclesTSP(X::Matrix{Float64}) = findSubcyclesTSP(getEdges(X), size(X, 1))
--- CNMAC 2024/tsp.jl
+++ CNMAC 2024/tsp.jl
@@ -41,29 +41,6 @@ function getEdges(X::Matrix{Float64})
     return edges
 end
 
-function findSubcyclesTSP(edges::Vector{Tuple{Int, Int}}, n::Int)
-    unvisited = Set(collect(1:n))
-    shortest = collect(1:n)
-    while !isempty(unvisited)
-        thisCycle, neighbors = Int[], unvisited
-        while !isempty(neighbors)
-            current = pop!(neighbors)
-            push!(thisCycle, current)
-            if length(thisCycle) > 1
-                pop!(unvisited, current)
-            end
-            neighbors =
-                [j for (i, j) in edges if i == current && j in unvisited]
-        end
-        if 1 < length(thisCycle) < length(shortest)
-            shortest = thisCycle
-        end
-    end
-    return shortest
-end
-
-findSubcyclesTSP(X::Matrix{Float64}) = findSubcyclesTSP(getEdges(X), size(X, 1))
-
 function graphTSP(X::Array, routecolor, x, y; linetype="curve", saveas="", nodesize=1.0)
     n = size(X, 1)
     g = SimpleDiGraph(n)
